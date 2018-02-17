f06f44a41441beea03a81043a982d3b39c5e3bdf
jmlim <jmlim@os.korea.ac.kr>
2018-02-17 18:55:54 +0900

    add UUID generation for IoT device

--- IoT_device.py
+++ IoT_device.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from uuid import uuid4
 from netifaces import interfaces, ifaddresses, AF_INET
 
 class DeviceInfo:
@@ -12,13 +13,14 @@ class DeviceInfo:
         self.trader_port = []
         self.device_ip = []
         self.device_port = []
-
+        # get IoT device`s IP address
         _iface = interfaces()
         _iface.remove(u'lo')
         _iface = ', '.join(_iface)
         _tmp = [i['addr'] for i in ifaddresses(_iface).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
         self.device_ip = ', '.join(_tmp)
-
+        # create UUID for IoT device
+        self.UUID = str(uuid4())
 
 
 if __name__ == '__main__':
@@ -31,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('-tip', '--tip', help='Trader`s ip address ', default='127.0.0.1')
     parser.add_argument('-tp', '--tport', help='Trader`s port number', default=5000, type=int)
     parser.add_argument('-m', '--model', default='JML_MK1', help='IoT device model name')
-    parser.add_argument('-s', '--sender', help='UUID of IoT device', default='83765f2d-49c8-4d74-95b2-2486116e7101')
+    #parser.add_argument('-s', '--sender', help='UUID of IoT device', default='83765f2d-49c8-4d74-95b2-2486116e7101')
     parser.add_argument('-f', '--firmware_hash', help='Hash value of firmware', default='773c839d24cf91c394aca6f1b9cd40da')
     parser.add_argument('-v', '--version', help='firmware version in string', default='Ubuntu 17.10')
     parser.add_argument('-ip', '--ip', help='This device IP', default='127.0.0.1')
@@ -44,7 +46,7 @@ if __name__ == '__main__':
     device.trader_ip = args.tip
     device.trader_port = args.tport
     device.model_name = args.model
-    device.UUID = args.sender
+    #device.UUID = args.sender
     device.firmware_hash = args.firmware_hash
     device.firmware_version = args.version
 
