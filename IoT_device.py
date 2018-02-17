627f2c9be1362513f3b4b9b81d1b16b5e875f084
jmlim <jmlim@os.korea.ac.kr>
2018-02-17 18:49:05 +0900

    connection established with Trader! Great!

--- IoT_device.py
+++ IoT_device.py
@@ -1,14 +1,8 @@
-import hashlib
 import json
-from time import time
-from urllib.parse import urlparse
-from uuid import uuid4
-
 import requests
-from flask import Flask, jsonify, request
-import socket
+from netifaces import interfaces, ifaddresses, AF_INET
 
-class device_info:
+class DeviceInfo:
     def __init__(self):
         self.model_name = []
         self.firmware_version = []
@@ -16,6 +10,15 @@ class device_info:
         self.UUID = []
         self.trader_ip = []
         self.trader_port = []
+        self.device_ip = []
+        self.device_port = []
+
+        _iface = interfaces()
+        _iface.remove(u'lo')
+        _iface = ', '.join(_iface)
+        _tmp = [i['addr'] for i in ifaddresses(_iface).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+        self.device_ip = ', '.join(_tmp)
+
 
 
 if __name__ == '__main__':
@@ -25,39 +28,46 @@ if __name__ == '__main__':
 
     # set default config
     # firmware
-    parser.add_argument('-ip', '--ip', help='Trader`s ip address ', default='127.0.0.1')
-    parser.add_argument('-p', '--port', help='Trader`s port number', default=5000, type=int)
+    parser.add_argument('-tip', '--tip', help='Trader`s ip address ', default='127.0.0.1')
+    parser.add_argument('-tp', '--tport', help='Trader`s port number', default=5000, type=int)
     parser.add_argument('-m', '--model', default='JML_MK1', help='IoT device model name')
     parser.add_argument('-s', '--sender', help='UUID of IoT device', default='83765f2d-49c8-4d74-95b2-2486116e7101')
     parser.add_argument('-f', '--firmware_hash', help='Hash value of firmware', default='773c839d24cf91c394aca6f1b9cd40da')
     parser.add_argument('-v', '--version', help='firmware version in string', default='Ubuntu 17.10')
+    parser.add_argument('-ip', '--ip', help='This device IP', default='127.0.0.1')
+    parser.add_argument('-p', '--port', help='This device port number', default=5100, type=int)
 
     args = parser.parse_args()
 
-    device_info.trader_ip = args.ip
-    device_info.trader_port = args.port
-    device_info.model_name = args.model
-    device_info.UUID = args.sender
-    device_info.firmware_hash = args.firmware_hash
-    device_info.firmware_version = args.version
+    device = DeviceInfo()
+
+    device.trader_ip = args.tip
+    device.trader_port = args.tport
+    device.model_name = args.model
+    device.UUID = args.sender
+    device.firmware_hash = args.firmware_hash
+    device.firmware_version = args.version
 
     print("IoT node information")
-    print("\t MODEL: " + device_info.model_name)
-    print("\t SENDER(IoT device) UUID: " + device_info.UUID)
-    print("\t FIRMWARE HASH: " + device_info.firmware_hash)
-    print("\t FIRMWARE VERSION:" + device_info.firmware_version)
-    device_ip = socket.gethostbyname(socket.gethostname())
-    print("\t IP address: " + device_ip)
+    print("\t MODEL: " + device.model_name)
+    print("\t SENDER(IoT device) UUID: " + device.UUID)
+    print("\t FIRMWARE HASH: " + device.firmware_hash)
+    print("\t FIRMWARE VERSION:" + device.firmware_version)
+    print("\t IP address: " + device.device_ip)
 
     print("Connection check")
-    trader_url = "http://" + device_info.trader_ip + ":" + str(device_info.trader_port) + "/nodes/device"
+    trader_url = "http://" + device.trader_ip + ":" + str(device.trader_port) + "/nodes/device"
     print("\t URL: " + trader_url)
 
     # must fix port number
     data = {
-        'ip': device_ip,
-        'UUID': device_info.UUID,
+        'ip': device.device_ip,
+        'port': '111',
+        'UUID': device.UUID,
     }
 
-    response = requests.post(trader_url, data=json.dumps(data))
-    print(response.text)
+    print(json.dumps(data))
+
+    response = requests.post(trader_url, json=data)
+    if response.ok:
+        print(response.text)
